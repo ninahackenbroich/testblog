36c557ca3bdde66e4654192020b112efd795758c
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2022-02-16 13:33:37 +0100

    added dotnec - Protect my secret data in env

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
   resources :articles, except: :index
-  # ⚠️ don't forget to replace `articles_path` by `root_path` on all the "Back" `link_to`
+  # ⚠️ don't forget to replace `root_path` by `root_path` on all the "Back" `link_to`
 end
